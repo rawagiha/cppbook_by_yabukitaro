e6eb8eb6d7b122d4ea42761f4d57a7d85554f4b5
Kohei Hagiwara <kohei.hagiwara@stjude.org>
2021-03-27 18:56:27 -0500

    7.1.2-string

--- chapter_07/7.1.String/7.1.2-string.cpp
+++ chapter_07/7.1.String/7.1.2-string.cpp
@@ -14,6 +14,16 @@ int main() {
 
     cout << (str1 == str2) << endl;
     cout << (str1 == "Hello, World!") << endl;
+    
     cout << str1.compare("Hallo, World!") << endl;
+    cout << str1.compare("Hello, World!") << endl;
     cout << str1.compare("Hello, Wozld!") << endl;
+
+    cout << str1.find("World") << endl;
+    cout << str1.rfind("o") << endl;
+
+    str1.replace(7, 5, "Fort Lauderdale");
+    cout << str1 << endl;
+
+    cout << str1.substr(7, 16) << endl;
 }
