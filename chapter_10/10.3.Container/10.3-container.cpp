50c7cb85dcd191c1d715dbb279156330a454fd67
Kohei Hagiwara <kohei.hagiwara@stjude.org>
2021-06-12 08:15:18 -0500

    10.3

--- chapter_10/10.3.Container/10.3-container.cpp
+++ chapter_10/10.3.Container/10.3-container.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <set>
+#include <unordered_map>
 
 class Person {
     public:
@@ -22,6 +24,15 @@ bool operator < (const Person& lhs, const Person& rhs) {
     return lhs.name < rhs.name;
 }
 
+namespace std {
+    template<>
+    struct hash<Person> {
+        size_t operator () (const Person& p) const {
+            return hash<std::string> () (p.name) ^ hash<int> () (p.age);
+        }
+    };
+}
+
 int main() {
     std::vector<Person> five_people(5);
 
@@ -35,4 +46,20 @@ int main() {
     sort(people.begin(), people.end());
     for (auto& p : people) std::cout << p.name << ", ";
     std::cout << std::endl;
+
+    std::set<Person> people_set;
+    people_set.emplace("xy", 4);
+    people_set.emplace("ds", 43);
+    people_set.emplace("fg", 5);
+    for (auto& p : people_set) std::cout << p.name << ", ";
+    std::cout << std::endl;
+
+    std::unordered_map<Person, std::string> people_dict;
+    people_dict.emplace(Person("ff", 4), "this is a pen");
+    people_dict.emplace(Person("kkk", 6), "not and");
+    people_dict.emplace(Person("ee", 3),  "fort lauderdale");
+    
+    Person ee("ee", 3);
+
+    std::cout << people_dict[ee] << std::endl;
 }
