a4d41057a97fe085d876f8538c1f47a1c304147a
Kohei Hagiwara <kohei.hagiwara@stjude.org>
2021-07-06 10:21:41 -0500

    exercise

---
+++ chapter_10/10.Exercise/2.exercise_move_constructor.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <chrono>
+
+class X {
+public:
+    std::vector<double> vec;
+    X() = default;
+    X(const X& x) : vec(x.vec) {}
+
+    X(X&& x) noexcept : vec(move(x.vec)) {}
+};
+
+
+X f() {
+    X x;
+    x.vec.resize(1000000);
+    return x;
+}
+
+int main() {
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+    for (int i = 1; i < 1000; i++) X x(f());
+    std::chrono::steady_clock::time_point end= std::chrono::steady_clock::now();
+    std::cout << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << std::endl;
+}
